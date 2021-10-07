b7a2170f90cdf8a34cab5db0ca269eea51d56844
DoctorCJack <krepetsmisha@yahoo.com>
2021-10-06 23:44:12 -0400

    Greatly improved the carving process of melons

--- src/main/java/net/doctorcjack/jackomelons/JackOMelons.java
+++ src/main/java/net/doctorcjack/jackomelons/JackOMelons.java
@@ -3,6 +3,15 @@ package net.doctorcjack.jackomelons;
 import net.doctorcjack.jackomelons.block.ModBlocks;
 import net.doctorcjack.jackomelons.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,6 +32,15 @@ public class JackOMelons implements ModInitializer {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 
+		UseBlockCallback.EVENT.register((player, world, hand, pos) ->
+		{
+			if (!player.isSpectator() && player.isHolding(Items.SHEARS) && world.getBlockState(pos.getBlockPos()).isOf(Blocks.MELON))
+			{
+				player.getStackInHand(hand).damage(1, player, (p) -> {p.sendToolBreakStatus(hand);});
+			}
+			return ActionResult.PASS;
+		});
+
 		LOGGER.info("Hello Fabric world!");
-	}
+	};
 }
--- src/main/java/net/doctorcjack/jackomelons/mixin/MelonMixin.java
+++ src/main/java/net/doctorcjack/jackomelons/mixin/MelonMixin.java
@@ -1,26 +1,36 @@
 package net.doctorcjack.jackomelons.mixin;
 
+import net.doctorcjack.jackomelons.JackOMelons;
 import net.doctorcjack.jackomelons.block.CarvedMelon;
 import net.doctorcjack.jackomelons.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.MelonBlock;
 import net.minecraft.datafixer.fix.ChunkPalettedStorageFix;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.Shearable;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.ShearsItem;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+
+import javax.swing.text.html.parser.Entity;
 
 @Mixin(MelonBlock.class)
 public abstract class MelonMixin extends Block{
@@ -31,7 +41,13 @@ public abstract class MelonMixin extends Block{
 
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if(player.isHolding(Items.SHEARS)) {
-            world.setBlockState(pos, ModBlocks.CARVED_MELON.getDefaultState().with(CarvedMelon.FACING, player.getHorizontalFacing().getOpposite()));
+            if(hit.getSide().equals(Direction.UP) || hit.getSide().equals(Direction.DOWN)) {
+                world.setBlockState(pos, ModBlocks.CARVED_MELON.getDefaultState().with(CarvedMelon.FACING, player.getHorizontalFacing().getOpposite()));
+            } else {
+                world.setBlockState(pos, ModBlocks.CARVED_MELON.getDefaultState().with(CarvedMelon.FACING, hit.getSide()));
+            }
+            world.playSound(pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_PUMPKIN_CARVE, SoundCategory.BLOCKS, 1.0f, 1.0f, true);
+            world.spawnEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.MELON_SEEDS, 4)));
         }
         return super.onUse(state, world, pos, player, hand, hit);
     }
